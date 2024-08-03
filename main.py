cdcab935077d7bb0b72959088908085f07d7d812
SathvikNayak123 <sathviknayak2@gmail.com>
2024-08-03 16:30:22 +0530

    data_validation_done

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.satge02_data_validation import DataValidationPipeline
 
 STAGE_NAME=" data ingestion"
 try:
     logger.info(f"{STAGE_NAME} begins")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"{STAGE_NAME} completed ")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME=" data validation"
+try:
+    logger.info(f"{STAGE_NAME} begins")
+    obj = DataValidationPipeline()
     obj.main()
     logger.info(f"{STAGE_NAME} completed ")
 except Exception as e:
---
+++ src/mlProject/pipeline/satge02_data_validation.py
@@ -0,0 +1,15 @@
+from src.mlProject.config.configuration import ConfigurationManager
+from src.mlProject.components.data_validation import DataValidation
+from src.mlProject import logger
+
+STAGE_NAME=" data validation"
+
+class DataValidationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
