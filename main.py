d6c7770e271291890aca8a6bbbe94857e15573ad
SathvikNayak123 <sathviknayak2@gmail.com>
2024-08-03 22:11:42 +0530

    model_done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.mlProject import logger
 from src.mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.satge02_data_validation import DataValidationPipeline
 from src.mlProject.pipeline.stage03_data_transformation import DataTransformationPipeline
+from src.mlProject.pipeline.stage04_model_trainer import ModelTrainPipeline
+from src.mlProject.pipeline.stage05_model_evaluation import ModelEvaluationPipeline
 
 STAGE_NAME=" data ingestion"
 try:
@@ -27,6 +29,26 @@ STAGE_NAME=" data transformation"
 try:
     logger.info(f"{STAGE_NAME} begins")
     obj = DataTransformationPipeline()
+    obj.main()
+    logger.info(f"{STAGE_NAME} completed ")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="model training"
+try:
+    logger.info(f"{STAGE_NAME} begins")
+    obj = ModelTrainPipeline()
+    obj.main()
+    logger.info(f"{STAGE_NAME} completed ")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME=" data transformation"
+try:
+    logger.info(f"{STAGE_NAME} begins")
+    obj = ModelEvaluationPipeline()
     obj.main()
     logger.info(f"{STAGE_NAME} completed ")
 except Exception as e:
---
+++ src/mlProject/pipeline/stage05_model_evaluation.py
@@ -0,0 +1,15 @@
+from src.mlProject.config.configuration import ConfigurationManager
+from src.mlProject.components.model_evaluation import ModelEvaluation
+from src.mlProject import logger
+
+STAGE_NAME=" data ingestion"
+
+class ModelEvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
